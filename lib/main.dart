a062b7afd73e12f6ddf8f2cb66a0d845c3c5ebc2
bobobo <36957912+liuchuancong@users.noreply.github.com>
2018-09-01 23:07:58 +0800

    Update main.dart
        if input nothing will be add
    so add this 
        if(txt.length != 0){
          _message.insert(0, msg);
          }

--- lib/main.dart
+++ lib/main.dart
@@ -7,24 +7,23 @@ final ThemeData iOSTheme = new ThemeData(
   primaryColor: Colors.grey[400],
   primaryColorBrightness: Brightness.dark,
 );
-
+//定义主题
 final ThemeData androidTheme = new ThemeData(
   primarySwatch: Colors.blue,
   accentColor: Colors.green,
 );
 
-const String defaultUserName = "John Doe";
-
-void main() => runApp(new MyApp());
+const String defaultUserName = 'John Doe';
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext ctx) {
+  Widget build(BuildContext context) {
     return new MaterialApp(
-      title: "Chat Application",
-      theme: defaultTargetPlatform == TargetPlatform.iOS
-        ? iOSTheme
-        : androidTheme,
+      title: 'Chat Application',
+      theme: defaultTargetPlatform == TargetPlatform.android
+          ? androidTheme
+          : iOSTheme,
       home: new Chat(),
     );
   }
@@ -36,79 +35,79 @@ class Chat extends StatefulWidget {
 }
 
 class ChatWindow extends State<Chat> with TickerProviderStateMixin {
-  final List<Msg> _messages = <Msg>[];
+  final List<Msg> _message = <Msg>[];
   final TextEditingController _textController = new TextEditingController();
   bool _isWriting = false;
-
   @override
-  Widget build(BuildContext ctx) {
+  Widget build(BuildContext context) {
+    // TODO: implement build
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Chat Application"),
-        elevation:
-          Theme.of(ctx).platform == TargetPlatform.iOS ? 0.0 : 6.0,
+        title: new Text('Chat Application'),
+        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 6.0,
       ),
       body: new Column(children: <Widget>[
         new Flexible(
-            child: new ListView.builder(
-              itemBuilder: (_, int index) => _messages[index],
-              itemCount: _messages.length,
-              reverse: true,
-              padding: new EdgeInsets.all(6.0),
-            )),
+          child: new ListView.builder(
+            itemBuilder: (_, int index) => _message[index],
+            itemCount: _message.length,
+            reverse: true,
+            padding: new EdgeInsets.all(6.0),
+          ),
+        ),
         new Divider(height: 1.0),
         new Container(
           child: _buildComposer(),
-          decoration: new BoxDecoration(color: Theme.of(ctx).cardColor),
+          decoration: new BoxDecoration(
+            color: Theme.of(context).cardColor,
+          ),
         ),
       ]),
     );
   }
 
-
   Widget _buildComposer() {
-    return new IconTheme(
-        data: new IconThemeData(color: Theme.of(context).accentColor),
-        child: new Container(
+    return IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
           margin: const EdgeInsets.symmetric(horizontal: 9.0),
-          child: new Row(
-            children: <Widget>[
-              new Flexible(
-                  child: new TextField(
-                    controller: _textController,
-                    onChanged: (String txt) {
-                      setState(() {
-                        _isWriting = txt.length > 0;
-                      });
-                    },
-                    onSubmitted: _submitMsg,
-                    decoration:
-                      new InputDecoration.collapsed(hintText: "Enter some text to send a message"),
-                  ),
+          child: new Row(children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onChanged: (String txt) {
+                  setState(() {
+                    _isWriting = txt.length > 0;
+                  });
+                },
+                onSubmitted: _submitMsg,
+                decoration: new InputDecoration.collapsed(
+                    hintText: '请输入消息'),
               ),
-              new Container(
+            ),
+            new Container(
                 margin: new EdgeInsets.symmetric(horizontal: 3.0),
                 child: Theme.of(context).platform == TargetPlatform.iOS
-                  ? new CupertinoButton(
-                    child: new Text("Submit"),
-                    onPressed: _isWriting ? () => _submitMsg(_textController.text)
-                        : null
-                )
+                    ? new CupertinoButton(
+                        child: new Text('Submit'),
+                        onPressed: _isWriting
+                            ? () => _submitMsg(_textController.text)
+                            : null,
+                      )
                     : new IconButton(
-                    icon: new Icon(Icons.message),
-                    onPressed: _isWriting
-                      ? () => _submitMsg(_textController.text)
-                        : null,
-                )
-              ),
-            ],
-          ),
+                        icon: new Icon(Icons.send),
+                        onPressed: _isWriting
+                            ? () => _submitMsg(_textController.text)
+                            : null,
+                      )),
+          ]),
           decoration: Theme.of(context).platform == TargetPlatform.iOS
-          ? new BoxDecoration(
-            border:
-              new Border(top: new BorderSide(color: Colors.brown))) :
-              null
-        ),
+              ? new BoxDecoration(
+                  border: new Border(
+                    top: new BorderSide(color: Colors.brown[200]),
+                  ),
+                )
+              : null),
     );
   }
 
@@ -120,33 +119,34 @@ class ChatWindow extends State<Chat> with TickerProviderStateMixin {
     Msg msg = new Msg(
       txt: txt,
       animationController: new AnimationController(
-          vsync: this,
-        duration: new Duration(milliseconds: 800)
+        vsync: this,
+        duration: new Duration(milliseconds: 800),
       ),
     );
     setState(() {
-      _messages.insert(0, msg);
+      if(txt.length != 0){
+      _message.insert(0, msg);
+      }
+
     });
     msg.animationController.forward();
   }
 
   @override
   void dispose() {
-    for (Msg msg in _messages) {
+    for (Msg msg in _message) {
       msg.animationController.dispose();
     }
     super.dispose();
   }
-
 }
 
 class Msg extends StatelessWidget {
   Msg({this.txt, this.animationController});
   final String txt;
   final AnimationController animationController;
-
   @override
-  Widget build(BuildContext ctx) {
+  Widget build(BuildContext context) {
     return new SizeTransition(
       sizeFactor: new CurvedAnimation(
           parent: animationController, curve: Curves.easeOut),
@@ -164,14 +164,14 @@ class Msg extends StatelessWidget {
               child: new Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  new Text(defaultUserName, style: Theme.of(ctx).textTheme.subhead),
+                  new Text(defaultUserName,
+                      style: Theme.of(context).textTheme.subhead),
                   new Container(
-                    margin: const EdgeInsets.only(top: 6.0),
-                    child: new Text(txt),
-                  ),
+                      margin: const EdgeInsets.only(top: 6.0),
+                      child: new Text(txt))
                 ],
               ),
-            ),
+            )
           ],
         ),
       ),
